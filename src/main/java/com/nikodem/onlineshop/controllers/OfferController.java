b8ea871a4a07bb054bfd5ee2190b7142db508572
arko0x <arkadiusz88084@gmail.com>
2020-09-02 12:09:36 +0200

    added offer validation

--- src/main/java/com/nikodem/onlineshop/controllers/OfferController.java
+++ src/main/java/com/nikodem/onlineshop/controllers/OfferController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import com.nikodem.onlineshop.repositories.OfferRepository;
 
+import javax.validation.Valid;
 import java.util.Date;
 
 @Controller
@@ -41,7 +43,11 @@ public class OfferController {
     }
 
     @PostMapping("/add")
-    public String proceedOffer(@ModelAttribute Offer offer, @AuthenticationPrincipal User user, Model model) {
+    public String proceedOffer(@ModelAttribute @Valid Offer offer, BindingResult bindingResult, @AuthenticationPrincipal User user, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "offerAdd";
+        }
+
         offer.setUser(user);
         offer.setPlacedAt(new Date());
         offerRepository.save(offer);
--- src/main/java/com/nikodem/onlineshop/domain/Offer.java
+++ src/main/java/com/nikodem/onlineshop/domain/Offer.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -14,13 +16,21 @@ public class Offer {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    @Size(max = 100)
+    @NotBlank
     private String name;
 
     @Column(columnDefinition="TEXT")
+    @NotBlank
+    @Size(max = 400)
     private String description;
 
+    @NotBlank
+    @Size(max = 30)
     private String whereItIs;
 
+    @NotBlank
+    @Size(max = 100)
     private String whyForSale;
 
     private Boolean isForNegotiation;
